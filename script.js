44f2ce61f7a02a4ed1f6b5151960c5aa20001db7
Sebastian Rodriguez <sebastianrodrbrens@gmail.com>
2024-06-29 16:45:02 -0600

    Add button to create different sizes of grids

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
+// This section is for the code needed add and hover effect over the grid
+
+let container = document.querySelector('.container'); // select the container
+container.addEventListener('mouseover', (event) => {
+    event.target.style.backgroundColor = '#666'; // highlights a square when the mouse hovers it
+});
+
+
+// This section is for the code needed to create the grid 
 
 function insertDiv(row) {
 
     let newDiv = document.createElement('div'); // create the new div
     newDiv.className = "divGrid";
     newDiv.style.border = "1px solid grey";
-    newDiv.style.height = "35px";
-    newDiv.style.width = "35px";
+    newDiv.style.height = "15px";
+    newDiv.style.width = "15px";
 
     row.appendChild(newDiv);
 
@@ -30,19 +39,35 @@ function generateRows() {
     return ''
 }
 
-const gridSize = 16
 
 
 function generateGrid() {
+
     let i = 0;
     
     while (i < gridSize) {
-        console.log(`he agregado el row ${i}`);
         generateRows();
         i++;
     }
 }
 
+// this section is for the button that generates larger grids
 
+let button = document.querySelector('#buttonGrid')
+button.addEventListener('click', () => {
+    let newGridSize = prompt(`How many squares do you want?\nCurrently there are ${gridSize} squares`)
+    if (newGridSize != "" && parseInt(newGridSize, 10) >= 1 && parseInt(newGridSize, 10) <= 100) {
+        gridSize = parseInt(newGridSize, 10);
+        let container = document.querySelector('.container'); // select the container
+        container.innerHTML = ""; // removes every child inside of container
+        generateGrid(); // regenerates the grid with the new number of rows-columns
+    } else{
+        alert("Invalid, try again");
+    }
+})
+
+
+
+let gridSize = 16
 generateGrid();
 
